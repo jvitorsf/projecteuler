809c0107bd27f52557f03650ef8abd325f94a560
João Vitor Sozinho Ferreira <jvitorsf@gmail.com>
2013-01-23 01:20:35 -0200

    Problems solved: 1,2,7

--- euler.py
+++ euler.py
@@ -1,44 +1,58 @@
-def fibonacci(n):
-	if(n ==0 or n==1):
-		return n;
-	else:
-		return fibonacci(n-1) + fibonacci(n-2);
+#!/usr/bin/python
 
+#Problem 1
+#Find the sum of all numbers multiples of 3 or 5 below 1000
+#Answer: 233168
+def multipleSum():
+	soma = 0;
+	for i in range(1000-1):
+		mult = i+1;
+		if(mult%3==0 or mult %5==0):
+			soma = soma + mult;
+	return soma;
+
+#Problem 2
+#Find the sum of the even-valued terms on Fibonacci sequence where the fibonacci value is less than 4 milions.
+#Answer: 4613732
+def fib(n):
+	if(n == 0 or n == 1):
+		return n
+	else:
+		return (fib(n-1)+fib(n-2))
+
+def problem2():
+	fibval = 0
+	soma = 0
+	cont = 0
+	while(fibval < 4000000):
+		fibval = fib(cont);
+		if(fibval % 2==0):
+			soma = soma + fibval
+		cont = cont + 1
+	return soma
+
+#Problem 7
+#Find the sum of the 10001st first primes
+#Answer: 104743
 def primeNumber(n):
-	primeCount=6;
-	number = 13;
-	lastPrime = 13;
-	flag = True;
+	primeCount=6
+	number = 13
+	lastPrime = 13
+	flag = True
 	while(primeCount != n):
-		number+=1;
+		number+=1
 		for i in range (number-1,2,-1):
 			if(number%i==0):
-				flag = False;
-				break;
+				flag = False
+				break
 		if(flag):
 			lastPrime = number;
-			primeCount+=1;
+			primeCount+=1
 		else:
-			flag = True;
+			flag = True
 
-	return lastPrime;
+	return lastPrime
 
-def recPrint(n):
-	print n;
-	if(n!=0):
-		recPrint(n-1);
+print(problem2())
 
-def foo1():
-	try:
-		number1 = int(raw_input("Enter a number: "));
-		number2 = int(raw_input("Enter another number: "));
-	except ValueError:
-		print ("Not a number");
-	
-	sum = number1+number2;
-	print sum;
-
-
-foo1();
-#print recPrint(1001);
 raw_input("\n\nPress enter to exit");
