4132b02c1bfee82f609c25b382a28857cb8291cf
João Vitor Sozinho Ferreira <jvitorsf@gmail.com>
2013-01-23 11:57:14 -0200

    Solution to problems 34 and 36

--- euler.py
+++ euler.py
@@ -53,6 +53,64 @@ def primeNumber(n):
 
 	return lastPrime
 
-print(problem2())
+#Problem 36
+#Find the sum of all numbers less than one million which are palindromic in base 10 and base 2
+#Answer: 872187
+def problem36():
+	cont = 0
+	soma = 0;
+	
+	while(cont<1000000):
+		#if cont equals to it's inverse (String)
+		if(str(cont) == str(cont)[::-1]):
+			#if cont (binarie) is equal to it's inverse (String)
+			revbin = bin(cont)[2:]
+			revbin = revbin[::-1]
+			if(str(bin(cont)[2:]) == str(revbin)):
+				soma = soma+cont;
+		cont = cont + 1
+	
+	return soma;
 
+#145: 1! + 4! +5! = 1 + 24 + 120 ==145
+#Find the sum of all number with this caracteristc (The sum of the factorial is equal to the number)
+#Answer: 40730
+def factorial(n):
+	fact = 0;
+	if(n<=1):
+		return 1
+	
+	fact = fact + n * factorial(n-1)
+	return fact
+
+def problem34():
+	num = 10
+	soma = 0
+	soma2 = 0
+	while(num <= 99999):
+		soma=0
+		numstr = str(num)
+		for i in range(0, len(numstr), 1):
+			fact = int(numstr[i])
+			#Checked the hint on the forum. 9! is a large number. The factorial sum won get this far.
+			if(fact<=8):
+				soma = soma + factorial(fact)
+				if(soma==num):
+					soma2 = soma2 +soma
+
+
+		num = num + 1
+	return soma2
+
+
+
+#Function Calls
+
+#multipleSum()
+#print(problem2())
+#primeNumber(10001)
+#print problem36()
+#print (factorial(5))
+#print factorial(6)
+print problem34()
 raw_input("\n\nPress enter to exit");
